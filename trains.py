7756e87c5cf1ffaa8b29db3127848d62f8c1739a
SJH07 <81580065+SJH07@users.noreply.github.com>
2021-03-29 17:33:09 +0100

    Update trains.py

--- trains.py
+++ trains.py
@@ -4,10 +4,12 @@ Created on Mon Mar 29 16:05:47 2021
 #Hello???
 @author: dob1u19
 """
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 mydata= pd.read_csv("C:\\Users\\Deepanshu\\Documents\\file1.csv")
 
-import matplotlib.pyplot as plt
 ul =  #u left
 vl=  #v left
 ur=  #u right
