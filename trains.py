fc9ca16e471241fc1a1e7cb43c07fbf7b0e8f79d
CecilieAndersen1 <71708311+CecilieAndersen1@users.noreply.github.com>
2021-03-29 17:36:59 +0100

    Update trains.py

--- trains.py
+++ trains.py
@@ -4,12 +4,15 @@ Created on Mon Mar 29 16:05:47 2021
 #Hello???
 @author: dob1u19
 """
+#Code for looking at train tracks from camera images
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Import data - file location needs changing
 mydata= pd.read_csv("C:\\Users\\Deepanshu\\Documents\\file1.csv")
 
+#plot points
 plt.plot(mydata.left_u,mydata.left_v)
 plt.plot(mydata.right_u,mydata.right_v)
 plt.show()
