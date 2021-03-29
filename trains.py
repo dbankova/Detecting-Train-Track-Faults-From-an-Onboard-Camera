553a46ef039ed9cd39cc57404086026c11eb8cb3
SJH07 <81580065+SJH07@users.noreply.github.com>
2021-03-29 17:28:54 +0100

    Update trains.py

--- trains.py
+++ trains.py
@@ -5,3 +5,12 @@ Created on Mon Mar 29 16:05:47 2021
 @author: dob1u19
 """
 
+import matplotlib.pyplot as plt
+ul =  #u left
+vl=  #v left
+ur=  #u right
+vr=  #v right
+
+plt.plot(ul,vl)
+plt.plot(ur,vr)
+plt.show()
