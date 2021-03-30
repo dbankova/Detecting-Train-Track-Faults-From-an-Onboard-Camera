fd5232319732b1d9f23f5790c4532dd9e4aab0ef
SJH07 <81580065+SJH07@users.noreply.github.com>
2021-03-30 15:14:02 +0100

    Update function.py

--- function.py
+++ function.py
@@ -26,10 +26,27 @@ def function(X, phi, theta, psi, f, Xd):
 
 g = 1.435 #track gauge
 w = 0.11 #track width
-xmax = 40 #max value of x we look ahead to
+h = 2.165 #camera height
+
+xd = 0 #x position of camera
+yd = 0.6 #y position of camera
+zd = 0 #z position of camera
+
+theta = -0.3333578871 #pitch angle
+phi = -0.1270599695 #yaw angle
+psi = 0 #roll angle (this will change with horizontal train vibrations)
+
+xmax = 40 #max value of x we look ahead to in metres
 x=np.linspace(0,xmax) #range from 0 to xmax metres
 
 ylo = g/2+w #left outer
 yli = g/2 #left inner
 yro = -g/2-w #right outer
 yri = -g/2 #right inner
+
+z = -h #we take camera to be the origin, so track is at -h
+
+Xlo = np.array([x,ylo,z])
+Xlo = np.array([x,ylo,z])
+Xlo = np.array([x,ylo,z])
+Xlo = np.array([x,ylo,z])
